3a2a087b602054c49c89256fa74735be646802d0
Anonnn888 <70950457+Anonnn888@users.noreply.github.com>
2020-09-09 13:51:05 +0700

    Add files via upload

---
+++ peledak.sh
@@ -0,0 +1,116 @@
+#
+#
+#
+#
+#
+#
+clear
+sleep 1
+cowsay -f dragon 'A̷N̷O̷N̷5̷4̷4̷' | lolcat
+#cowsay
+sleep 1
+echo "Gunakan Dengan Bijak" | lolcat
+sleep 1
+echo "Jika Tidak work Jangan Salahkan author" | lolcat
+sleep 1
+read -p "target>>>>" mm;
+sleep 1
+echo
+sleep 1
+echo "\033[1;32mINSYAALLAH WORK✓"
+sleep 1
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "Mengirim Virus Ke $mm"
+echo
+sleep 1
+echo "sekian"
+sleep 2
+
